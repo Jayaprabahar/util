85da99f5e66263c06fbd03fd9491b9d60c72e594
Jayaprabahar <jpofficial@gmail.com>
2022-06-21 19:39:53 +0200

    Added method for string to check whether it can be assignable to a java Class

--- src/main/java/com/jayaprabahar/util/json/JsonUtil.java
+++ src/main/java/com/jayaprabahar/util/json/JsonUtil.java
@@ -8,6 +8,9 @@ import java.util.List;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.experimental.UtilityClass;
 
 /**
  * <p> Project : com.jayaprabahar.util.json </p>
@@ -18,7 +21,10 @@ import com.google.gson.reflect.TypeToken;
  * @version 1.0.0
  * @author <a href="mailto:jpofficial@gmail.com">Jayaprabahar</a>
  */
+@UtilityClass
 public final class JsonUtil {
+	
+	public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
 	private JsonUtil() {
 		throw new IllegalStateException("Utility class");
@@ -44,5 +50,23 @@ public final class JsonUtil {
 	public static boolean isJsonElementEmpty(JsonElement jsonElement) {
 		return (jsonElement.isJsonNull() || !jsonElement.isJsonArray() || jsonElement.toString().equals("[]"));
 	}
+	
+	/**
+	 * Checks whether the given string is the json string form of the given class
+	 * 
+	 * @param String jsonString
+	 * @param Class assignableClass
+	 * 
+	 * @return boolean
+	 */
+	public boolean isAssignableTo(String jsonString, Class assignableClass) {
+	      try {
+		    OBJECT_MAPPER.readValue(jsonString, assignableClass);
+		    return true;
+	      } catch (JsonProcessingException e) {
+		 return false;
+	      }
+	}
+	
 
 }
